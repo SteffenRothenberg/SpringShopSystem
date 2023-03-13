d2bf5f27ac01d1f761cc1db073fb6327ec0a77cc
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2023-03-13 16:16:29 +0100

    init Commit

---
+++ SpringShopSystemApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.springshopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopSystemApplication.class, args);
+	}
+
+}
